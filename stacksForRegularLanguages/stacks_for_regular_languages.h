f994a4eb876c3ade710ec8e49f574ac6e872b070
Jessie Huang <jessiemh@ufl.edu>
2018-06-21 13:16:21 -0400

    6.21.2018: testing cases of given main.cpp file working

--- stacksForRegularLanguages/stacks_for_regular_languages.h
+++ stacksForRegularLanguages/stacks_for_regular_languages.h
@@ -20,23 +20,35 @@ public:
 
 template <class T>
 bool stacks_for_regular_languages<T>::L1(char* inputString) {
-	stack_p1<char> s1;
-	int n = sizeof(inputString) / sizeof(char);
-	for (int i = 0; i < n; ++i)
-	{
-		char c = inputString[i];
-		if (s1.isEmpty()) {
-			s1.push(c);
-			continue;
-		}
-		if (s1.peek() == c) {
-			s1.push(c);
-		}
-		else {
-			s1.pop();
+	stack_p1<char> sA;
+	char first = *inputString;
+
+	//if does not start with A, automatically false
+	if (first != 'A') return false;
+
+	//iterate string...push all A to stack, will use to track how many ordered Bs are in string
+	for (char *it = inputString; *it; ++it) {
+		//automatically false case
+		if ((*it != 'A') && (*it != 'B')) return false;
+
+		//found A...push to stack
+		if (*it == 'A') sA.push(*it);
+	}
+
+	//want: pop A as many times as you find B
+	//iterate string again...pop sA every time string lands on B
+	for (char *it = inputString; *it; ++it) {
+		if (*it != first) {
+			//if A stack is not empty and equal to B, pop
+			if (!sA.isEmpty()) sA.pop();
+			//else stack is empty and iterator lands on character not equal to A, language not valid
+			else return false;
 		}
 	}
-	return s1.isEmpty();
+
+	//empty stack = accepted language, else language false
+	if (sA.isEmpty()) return true;
+	else return false;
 }
 
 template <class T>
@@ -83,43 +95,109 @@ bool stacks_for_regular_languages<T>::L2(char* inputString) {
 
 template <class T>
 bool stacks_for_regular_languages<T>::L3(char* inputString) {
-	stack_p1<char> s1;
-	int n = sizeof(inputString) / sizeof(char);
-	for (int i = 0; i < n; ++i)
-	{
-		char c = inputString[i];
-		if (s1.isEmpty()) {
-			s1.push(c);
-			continue;
+	char first = *inputString;
+	stack_p1<std::string> pattern;
+	stack_p1<std::string> pattern2;
+	bool foundB = false;
+	std::string containsPattern="";
+	std::string checkStr="";
+
+	//if does not start with A, automatically false
+	if (first != 'A') return false;
+
+	//iterate string...find pattern, concentate pattern to string, push string to stack
+	for (char *it = inputString; *it; ++it) {
+		//automatically false case
+		if ((*it != 'A') && (*it != 'B')) return false;
+
+		//found A...start string concenation 
+		if (foundB == false) {
+			if (*it == 'A') containsPattern += *it;
+			else foundB = true;
 		}
-		if (s1.peek() == c) {
-			s1.push(c);
-		}
-		else {
-			s1.pop();
+
+		//if iterator is A, pattern is finished, push pattern to stacks for later
+		//else iterator is on a B, so continue string concenation
+		if (foundB == true) {
+			if (*it == 'A') {
+				pattern.push(containsPattern);
+				pattern2.push(containsPattern);
+				containsPattern = *it;   //reset
+				foundB = false;
+			}
+			else containsPattern += *it;
 		}
 	}
-	return s1.isEmpty();
+	//push last pattern repeat to stacks
+	pattern.push(containsPattern);
+	pattern2.push(containsPattern);
+
+	//concenate new string of last stack entry
+	while (!pattern.isEmpty()) checkStr += pattern.pop();
+
+	//WANT: inputString == postString
+	//if not equal return false
+	if (inputString != checkStr) return false;
+	
+	//WANT: each stack entry of pattern to pass l2
+	while(!pattern2.isEmpty()){
+		//get top entry of stack to push to l2 to check
+		if (!L2(&pattern2.pop().at(0))) return false;
+	}
+	
+	//if strings are equal and each individual pattern is l2 case, then return true
+	return true;
 }
 
 template <class T>
 bool stacks_for_regular_languages<T>::L4(char* inputString) {
-	stack_p1<char> s1;
-	int n = sizeof(inputString) / sizeof(char);
-	for (int i = 0; i < n; ++i)
-	{
-		char c = inputString[i];
-		if (s1.isEmpty()) {
-			s1.push(c);
-			continue;
+	char first = *inputString;
+	stack_p1<std::string> pattern;
+	stack_p1<std::string> pattern2;
+	bool foundB = false;
+	std::string containsPattern = "";
+	std::string checkStr = "";
+
+	//if does not start with A, automatically false
+	if (first != 'A') return false;
+
+	//iterate string...find pattern, concentate pattern to string, push string to stack
+	for (char *it = inputString; *it; ++it) {
+		//automatically false case
+		if ((*it != 'A') && (*it != 'B')) return false;
+
+		//found A...start string concenation 
+		if (foundB == false) {
+			if (*it == 'A') containsPattern += *it;
+			else foundB = true;
 		}
-		if (s1.peek() == c) {
-			s1.push(c);
-		}
-		else {
-			s1.pop();
+
+		//if iterator is A, pattern is finished, push pattern to stacks for later
+		//else iterator is on a B, so continue string concenation
+		if (foundB == true) {
+			if (*it == 'A') {
+				pattern.push(containsPattern);
+				pattern2.push(containsPattern);
+				containsPattern = *it;   //reset
+				foundB = false;
+			}
+			else containsPattern += *it;
 		}
 	}
-	return s1.isEmpty();
+	//push last pattern repeat to stacks
+	pattern.push(containsPattern);
+	pattern2.push(containsPattern);
+
+	//concenate new string of last stack entry
+	while (!pattern.isEmpty()) checkStr += pattern.pop();
+
+	//WANT: inputString == postString
+	//if not equal return false
+	if (inputString != checkStr) return false;
+
+	if (foundB == false) return false;
+
+	//if strings are equal and each individual pattern is l2 case, then return true
+	return true;
 }
 #endif /*stacks_for_regular_languages_h*/
